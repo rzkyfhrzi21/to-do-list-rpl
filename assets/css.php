11e4f031182df9a8e9fb887bb2141d27061bcf68
rzkyfhrzi21 <rzkyfhrzi21@gmail.com>
2026-01-14 19:58:50 +0700

    .

--- assets/css.php
+++
@@ -1,5 +0,0 @@
-<link rel="shortcut icon" href="assets/pmi-bg.png" type="image/x-icon">
-<link rel="shortcut icon" href="assets/pmi-bg.png" type="image/png">
-<link rel="stylesheet" href="../assets/compiled/css/app.css">
-<link rel="stylesheet" href="../assets/compiled/css/app-dark.css">
-<link rel="stylesheet" href="../assets/compiled/css/auth.css">
--- pages/css.php
+++ pages/css.php
@@ -7,7 +7,11 @@
 
 
 <!-- Font Awesome -->
-<link rel="stylesheet" href="assets/extensions/@fortawesome/fontawesome-free/css/all.min.css">
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer">
+
 <!-- Sweetalert -->
 <link rel="stylesheet" href="assets/extensions/sweetalert2/sweetalert2.min.css">
 <!-- Choices -->
